0122574fddc219b78efe950543a945d34c4cf1f2
Thawfi <thawfi786@gmail.com>
2022-09-22 16:01:53 +0530

    hello

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,40 @@
+package org.emp;
+
+public class Employee {
+	private void empId() {
+		System.out.println("123");
+		
+	}
+	private void empName() {
+		System.out.println("thawfeek");
+		
+	}
+	private void empDob() {
+		System.out.println("21/12/1996");
+		
+	}
+	private void empPhone() {
+		System.out.println("5542544445");
+		
+	}
+	private void empEmail() {
+		System.out.println("thawfi456@gmail.com");
+		
+	}
+	private void empAddress() {
+		System.out.println("kanyakumari");
+		
+	}
+	public static void main (String Args[]) {
+		Employee cardNum = new Employee();
+		cardNum.empId();
+		cardNum.empDob();
+		cardNum.empPhone();
+		cardNum.empEmail();
+		cardNum.empAddress();
+		
+		
+	}
+	
+
+}
